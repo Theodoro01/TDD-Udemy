ec088d0602155637ff4888ac2ec48e97036096dc
Theodoro01 <thedorovieira01@gmail.com>
2022-07-20 18:17:27 -0300

    async / await

--- test/geral.test.js
+++ test/geral.test.js
@@ -1,8 +1,19 @@
 let supertest = require("supertest")
 let request = supertest("www.google.com")
 
-test('A aplicação deve responder na porta 3131', () => {
+test('A aplicação deve responder na porta 3131', async () => {
+    // Promise 
+    // return request.get('/').then(res => expect(res.statusCode).toEqual(200))
 
-    return request.get('/').then(res => expect(res.statusCode).toEqual(200))
+    // Async Await
+
+    try{
+
+        let res = await request.get('/')
+        expect(res.statusCode).toEqual(200)
+        
+    }catch(err){
+        console.log(err)
+    }
 
 })
