2fcf3fb3023ff1e0253b3354bf9f423a75718b6b
Theodoro01 <thedorovieira01@gmail.com>
2022-07-20 18:49:09 -0300

    teste de cor

--- src/app.js
+++ src/app.js
@@ -3,4 +3,12 @@ const app = express()
 
 app.get('/', ( _, res ) => res.json({ msg: true }))
 
+app.get("/cor/:pessoa", (req, res) => {
+    let pessoa = req.params.pessoa
+
+    if(pessoa == "theo"){
+        res.json({cor: "vermelho"})
+    }
+})
+
 module.exports = app
--- test/geral.test.js
+++ test/geral.test.js
@@ -1,7 +1,11 @@
 const app = require("../src/app")
 let supertest = require("supertest")
-let request = supertest("app")
+let request = supertest(app)
 
 test('A aplicação deve responder na porta 3131', () => {
     return request.get('/').then(res => expect(res.statusCode).toEqual(200))
+})
+
+test('Deve retornar vermelho', () => {
+    return request.get('/cor/theo').then(res => expect(res.body.cor).toEqual("vermelho"))
 })
